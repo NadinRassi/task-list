ea771110c917f36b072afbd208f2bd5d38c51cec
Nadin Rassi <sofianadinrassi@gmail.com>
2022-03-22 08:57:39 -0300

    Add mock Tasks

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,29 @@
+import {Task} from "./task"
+export const TASKS:Task[] =[
+  {
+    id:1,
+    text:"Temrinar primer modulo de Angular",
+    day:"Agosto 5 a las 12:00",
+    reminder:true
+  },
+  {
+    id:2,
+    text:"Hacer compras",
+    day:"Agosto 5 a las 17:00",
+    reminder:true
+  },
+  {
+    id:3,
+    text:"Investigar sobre bootstrap",
+    day:"Agosto 5 a las 19:00",
+    reminder:false
+  },
+  {
+    id:4,
+    text:"Leer mi libro fav",
+    day:"Agosto 5 a las 21:00",
+    reminder:false
+  }
+
+
+]
